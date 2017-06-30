f2cebe1c979e1bafa4cdde91a824512c6f852b63
Amin <ameri.env@gmail.com>
2017-06-30 14:05:53 -0400

    Initial Commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,35 @@
+const {MongoClient, ObjectID} = require('mongodb');
+//OR
+//const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb://localhost:27017/ToDoApp', (err, db) => {
+    if(err){
+        return console.log(`Error happened ${err}`);
+        //return does not do anything. just does not go further
+        //Or we can use if-else
+    }
+    //Insert a todo document
+    // db.collection('Todos').insertOne({
+    //     text: 'Learn MongoDB',
+    //     completed: false
+    // }, (err, result) => {
+    //     if(err){
+    //         return console.log('Unable to insert', err);
+    //     }
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    // });
+
+    //Insert a user document
+    db.collection('Users').insertOne({
+        name: 'Ayako',
+        age: 30,
+        location: 'Tokyo'
+    }, (err, result) => {
+        if(err){
+            return console.log('Unable to insert into Users', err);
+        }
+        console.log(JSON.stringify(result.ops, undefined, 2));
+        console.log(result.ops[0]._id.getTimestamp());
+    });
+    
+    db.close();
+});
---
+++ playground/mongodb-find.js
@@ -0,0 +1,27 @@
+const {MongoClient, ObjectID} = require('mongodb');
+//OR
+//const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb://localhost:27017/ToDoApp', (err, db) => {
+    if(err){
+        return console.log(`Error happened ${err}`);
+        //return does not do anything. just does not go further
+        //Or we can use if-else
+    }
+    db.collection('Todos').find(
+            {_id: new ObjectID('59566f1129066a05c0b0b8f3')}
+        ).toArray().then((result) => {
+        console.log(JSON.stringify(result, undefined, 2));
+    }, (err) => {
+        console.log('Error happened', err);
+    });
+
+    db.collection('Users').find({
+        name: 'Ayako'
+    }).count().then((countNo) => {
+        console.log(`User's Count: ${countNo}`);
+    }, (err) => {
+        console.log('Error happened:', err);
+    });
+    
+    db.close();
+});
