ddd07f85357fa8576d58d4f2e29a32e6420b4fd6
Amin <ameri.env@gmail.com>
2017-07-04 18:54:10 -0400

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,14 +4,26 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+        text: 'First test todo'
+    }, {
+        text: 'Second test todo'
+    }];
+
+// beforeEach( (done) => {
+//     Todo.remove({}).then( () => {
+//         done();
+//     })
+// });
+//Above should be modified as it always remove everything in the collection so that is not really suitable approach
 
 beforeEach( (done) => {
     Todo.remove({}).then( () => {
-        done();
-    })
-});
+        return Todo.insertMany(todos);
+    }).then(() =>  done());
+})
 
-describe('POST / TODOS', () =>{
+describe('POST / todos', () =>{
 
     it('should create a new TODO', (done) => {
         var text = 'Text todo text';
@@ -30,7 +42,7 @@ describe('POST / TODOS', () =>{
                     return done(err);
                 }
 
-                Todo.find().then( (todos) => {
+                Todo.find({text}).then( (todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -49,11 +61,22 @@ describe('POST / TODOS', () =>{
                     return done(err);
                 }
                 Todo.find().then( (todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 }).catch( (e) => {
                     done(e);
-                })
+                });
+            });
+    });
+});
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect( (res) => {
+                expect(res.body.todos.length).toBe(2);
             })
+            .end(done);
     });
 });
